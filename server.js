33ca4d9ae825f640406f330cfa66a84e53b99737
Howaida <howaidasayed95@gmail.com>
2025-07-20 03:10:05 +0300

    add cloudinary & db config to server file

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 // app config
 /*
 it returns an Express application instance (app), which acts as the backbone of your
@@ -13,10 +14,11 @@ it provides methods to:
 */
 const app = express();
 const port = process.env.PORT || 4000;
-
+connectDB();
+connectCloudinary();
 // middleware
+app.use(cors({})); // so we can access backend from any ip
 app.use(express.json());
-app.use(cors()); // so we can access backend from any ip
 
 // api endpoints
 app.get("/", (req, res) => {
@@ -24,6 +26,6 @@ app.get("/", (req, res) => {
 });
 
 // start the server
-app.listen(port, () => {
+app.listen(4000, () => {
     console.log(`server started on port ${port}`);
 });
